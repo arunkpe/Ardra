ca5fad5e99742eb9f2d0174ad5bb2eda5f0dc40b
arunkpe <arunkpe@yahoo.com>
2015-01-12 01:46:34 -0800

    Main Code

---
+++ Code/RatingMA.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+import pandas as pd
+import numpy as np
+
+baco = pd.read_csv('review_guisados-los-angeles.csv', encoding='cp1252')
+
+print(baco)
